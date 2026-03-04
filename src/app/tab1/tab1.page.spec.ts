e22fa16e65890e3cd8f77399970843a2c618a490
Dhineshkumar Ashokkumar <dhinesh.a@ASE.LAN>
2026-03-04 12:55:11 +0530

    Initial setup 2

--- src/app/tab1/tab1.page.spec.ts
+++ src/app/tab1/tab1.page.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
+import { Storage } from '@ionic/storage-angular';
 
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
@@ -12,7 +13,17 @@ describe('Tab1Page', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [Tab1Page],
-      imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
+      imports: [IonicModule.forRoot(), ExploreContainerComponentModule],
+      providers: [
+        {
+          provide: Storage,
+          useValue: {
+            create: jasmine.createSpy('create').and.resolveTo(),
+            get: jasmine.createSpy('get').and.resolveTo(null),
+            set: jasmine.createSpy('set').and.resolveTo(),
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Tab1Page);
