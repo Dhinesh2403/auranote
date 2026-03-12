e672476d9ae5e257da09902a8d7740baefc7898b
Dhinesh2403 <Dhinesh2403@users.noreply.github.com>
2026-03-12 11:46:11 +0000

    deploy: 866d33d7bc51f1cda8ff148ef51bba33236a20a3

--- assets/env.js
+++ assets/env.js
@@ -1,4 +1,5 @@
-(function () {
+(function (window) {
   window.__ENV = window.__ENV || {};
-  window.__ENV.AURA_ENDPOINT = "";
-})();
+  window.__ENV.AURA_ENDPOINT = 'https://models.github.ai/inference';
+  window.__ENV.AURA_API_KEY = atob('Z2hwX0xZNFZDa29EVGhLaEZoWlBudG9XTG1XWFYzT3JEUjRicnZrMw==');
+})(this);
